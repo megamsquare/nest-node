bf74b302adaf9fb8346d58634217a462aa20442e
Michael <msquaremega@gmail.com>
2023-03-13 15:36:24 +0100

    create user modeul

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './v1/auth/auth.module';
+import { UserModule } from './v1/user/user.module';
 
 @Module({
   imports: [AuthModule, UserModule],
