6f1f04b1fe27ba8c9a22c4bc6a576ab426267e14
Michael <msquaremega@gmail.com>
2023-03-13 15:54:03 +0100

    remove controller mistake

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,14 +3,13 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './v1/auth/auth.module';
 import { UserModule } from './v1/user/user.module';
-import { GitController } from './add/git/git.controller';
 
 @Module({
   imports: [
     AuthModule, 
     UserModule
   ],
-  controllers: [AppController, GitController],
+  controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
